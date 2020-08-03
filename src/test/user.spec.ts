5c2bbf9a869d51561858918910e5c5e31a81a1d9
seungbin <tanja1206@naver.com>
2020-08-03 22:11:48 +0900

    user test

--- src/test/user.spec.ts
+++ src/test/user.spec.ts
@@ -6,21 +6,7 @@ import app from "../app";
 const expect = chai.expect;
 chai.use(chaiHttp);
 
-describe('User', () => {
-    it('POST /user/register (200)', (done) => {
-        chai
-            .request(app)
-            .post("/user/register")
-            .send({
-                name: "name",
-                userId: "userId",
-                password: "password",
-            })
-            .end((err, res) => {
-                expect(res).to.have.status(200);
-                done();
-            })
-    });
+describe('User Spec Test', () => {
     it('POST /user/register (409)', (done) => {
         chai
             .request(app)
@@ -32,6 +18,19 @@ describe('User', () => {
             })
             .end((err, res) => {
                 expect(res).to.have.status(409);
+                done();
+            })
+    });
+    it('POST /user/login (200)', (done) => {
+        chai
+            .request(app)
+            .post("/user/login")
+            .send({
+                userId: "userId",
+                password: "password",
+            })
+            .end((err, res) => {
+                expect(res).to.have.status(200);
                 done();
             })
     })
