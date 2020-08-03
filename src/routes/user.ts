c34077f3f13b5b6fa7cd3398ec53c86d000a1309
seungbin <tanja1206@naver.com>
2020-08-03 22:11:42 +0900

    connect router

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -5,5 +5,6 @@ import * as User from "../controller/user/user.controller";
 const router = express();
 
 router.post('/register', tryCatchMiddleware.Conflict(User.register));
+router.post('/login', tryCatchMiddleware.NotFound(User.login));
 
 export default router;
