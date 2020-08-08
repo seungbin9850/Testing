0edb47f3737334edd53464617cd1ac81b8c15074
seungbin <tanja1206@naver.com>
2020-08-08 22:31:45 +0900

    main

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ import express, { Request, Response, NextFunction } from "express";
 import { sequelize } from "./config/config";
 import morgan from "morgan";
 import router from "./routes";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join(__dirname, "../../.env") });
 
 const app = express();
 
@@ -11,6 +15,8 @@ sequelize.sync();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.set('jwt-secret', process.env.JWT_SECRET);
 
 app.use('/', router)
 
