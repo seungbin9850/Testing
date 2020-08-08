e4a886c7e379f4b20d7bb4c851c0ee94c2e96efc
seungbin <tanja1206@naver.com>
2020-08-08 22:32:47 +0900

    user controller

--- src/controller/user/user.controller.ts
+++ src/controller/user/user.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import * as query from "./query";
+import { mkAccess } from "./mkToken";
 
 export const register = async (req: Request, res: Response) => {
     const { name, userId, password } = req.body;
@@ -11,9 +12,13 @@ export const register = async (req: Request, res: Response) => {
 
 export const login = async (req: Request, res: Response) => {
     const { userId, password } = req.body;
-    const user = await query.findOneById(userId);
+    const user: any = await query.findOneById(userId);
     if (user.password !== password)
         res.status(403).json({ message: "실패" });
-    else
-        res.status(200).json({ message: "성공" });
+    else {
+        const accessToken = await mkAccess(req, user);
+        user.accessToken = accessToken;
+        user.save();
+        res.status(200).json({ accessToken, message: "성공" });
+    }
 }
