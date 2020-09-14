3a2e0b4288d8d9c0cfc6232b48c86687e7f011c2
seungbin <tanja1206@naver.com>
2020-09-14 19:06:37 +0900

    post query

--- src/controller/post/query.ts
+++ src/controller/post/query.ts
@@ -6,6 +6,6 @@ export const mkId = async (): Promise<string> => {
     return id[2] + id[1] + id[0] + id[3] + id[4]
 }
 
-export const writeOne = async (id: string, title: string, content: string, writerId: string) => {
-    await Post.create({ id, title, content, writerId });
+export const writeOne = async (id: string, title: string, content: string, writerId: string): Promise<Post> => {
+    return await Post.create({ id, title, content, writerId });
 }
