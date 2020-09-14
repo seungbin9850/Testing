c58386a7e17608b7f62192f77171cab481a65a1a
seungbin <tanja1206@naver.com>
2020-09-14 19:06:30 +0900

    post controller

--- src/controller/post/post.controller.ts
+++ src/controller/post/post.controller.ts
@@ -3,8 +3,8 @@ import * as query from "./query";
 
 export const write = async (req: Request, res: Response) => {
     const { title, content } = req.body;
-    const writerId = req["decoded"].id;
-    const id = await query.mkId();
+    const writerId: string = req["decoded"].id;
+    const id: string = await query.mkId();
     await query.writeOne(id, title, content, writerId);
     res.status(200).json({ message: "성공" });
 }
